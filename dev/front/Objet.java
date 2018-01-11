99381e58aa3a3181c3b6fe9361bcdea4b5a2054d
Unknown <alexisbln44@gmail.com>
2018-01-11 14:41:07 +0100

    création objet

---
+++ dev/front/Objet.java
@@ -0,0 +1,42 @@
+public class Tuile {
+
+}
+
+public class Troupe extends Tuile {
+  public String Nom;
+  public char ModeMouvement;
+  public String LocalisationBase;
+  public String TypeArmement;
+  public String TypeTroupe;
+  public int Deplacement;
+  public int Moral;
+  public int ValeurMele;
+  public int ValeurMissile;
+  public String EtatArmure;
+}
+
+public class Personnage extends Tuile {
+  public String Nom;
+  public char ModeMouvement;
+  public String LocalisationBase;
+  public int NiveauMagiePuissance;NiveauHabilité
+  public int NiveauHabilité;
+  public int Deplacement;
+  public String Type;
+}
+
+public class Hexagone {
+
+  public String Territoire;
+  public String Occupation;
+  public String SousTypeTerrainArrete1;
+  public String SousTypeTerrainArrete2;
+  public String SousTypeTerrainArrete3;
+  public String SousTypeTerrainArrete4;
+  public String SousTypeTerrainArrete5;
+  public String SousTypeTerrainArrete6;
+  public String TypeTerrain;
+  public Tuile Tuiles;
+  public int CoordonnéeX;
+  public int CoordonnéeY;
+}
